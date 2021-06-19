8746d614c6ef3b22d7d2e65319b618bd5be71977
sunitee <suniteebajpai2@gmail.com>
2021-06-19 19:02:24 -0400

    added Home Page

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import {Jumbotron} from 'react-bootstrap'
+
+
+const Home = () => {
+
+  return (
+    <div className='homepage'>
+        <Jumbotron fluid className='jumbotron'>
+          <h1>Welcome to ClassicCars </h1>
+          <h2>Vehicle Inventory System</h2>
+          </Jumbotron>
+    </div>
+  )
+}
+export default Home
