d8b27ea960a504630ea69cbeb6bc7418f5bb2f8e
sunitee <suniteebajpai2@gmail.com>
2021-06-19 18:52:09 -0400

    added About page

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+
+  return (
+    <div className='about'>
+      <div>
+
+      <p> Add Car To Your Collection!!</p>
+      </div>
+    </div>
+  )
+}
+
+export default About
