4013e77b71b4bcdb218ba6ab3de98592eacb5b4b
sunitee <suniteebajpai2@gmail.com>
2021-06-19 18:30:06 -0400

    App.js setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-
+import React,{ Component } from 'react';
 import './App.css';
 
-function App() {
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+class App extends Component {
+  render(){
   return (
     <div className="App">
+
+    <h1>Classic Cars</h1> 
       
     </div>
   );
+  }
 }
 
 export default App;
