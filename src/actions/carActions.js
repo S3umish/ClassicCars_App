6bbaa4b5d44d24dc87866fe5fa22a3d2af0a2fb3
sunitee <suniteebajpai2@gmail.com>
2021-06-20 07:31:14 -0400

    setting up state change

--- src/actions/carActions.js
+++ src/actions/carActions.js
@@ -1,14 +1,19 @@
 const url = "http://localhost:3001/cars"
 
 
+export const addCars = (cars) => ({type: "ADD_CARS", payload: cars})
+
+
+
 export const fetchCars = () => {
     
     return (dispatch) => {
 
         fetch(url)
         .then(res => res.json())
-        .then(json => {
-            console.log(json)
+        .then(cars => {
+            // console.log(cars)
+            dispatch(addCars(cars))
         })
 
     }
--- src/reducers/carReducer.js
+++ src/reducers/carReducer.js
@@ -1,6 +1,13 @@
 export default function carReducer(state = {cars: []}, action){
 
     switch (action.type){
+        case "ADD_CARS":
+            return{
+                ...state,
+                cars: action.payload
+            }
+
+
 
         default:
             return state
