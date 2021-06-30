a4b582a200e78204b3e5084d31ab176c44e24fc4
sunitee <suniteebajpai2@gmail.com>
2021-06-29 21:12:25 -0400

    updated About

--- src/components/About.js
+++ src/components/About.js
@@ -5,11 +5,14 @@ const About = () => {
   return (
     <div className='about'>
 
-      <h6>Welcome to a collection of automobiles that illustrate the evolution and art of the automobile and its role in our lives. </h6>
-      <h6>The collection is diverse and includes cars from many different eras</h6>
-      <h6>Classic Car *Collection* is dedicated to presenting an entertaining yet artful perspective tracing the major role that the automobile has played relative to history, culture, industry, and innovation.</h6>
-      <h6>By visiting the Classic Car Collection, you will be participating in a very important educational mission: celebrating the automobile as a vital element of art, industry, freedom.</h6>  
+      <p>Welcome to a collection of automobiles.</p>
+
+      <h6>The collection is diverse and includes cars from many different eras.</h6>
+      
       <h6>Visit our Classic Cars Collection today !!</h6>
+
+      <img src="https://png.clipart.me/previews/4cb/retro-sport-car-35221.jpg" alt="car" />
+
       </div>
    
   )
