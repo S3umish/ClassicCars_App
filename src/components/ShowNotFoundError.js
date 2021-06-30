c12f5abdb616e2d137306c5052fd4cff0bcc3c3d
sunitee <suniteebajpai2@gmail.com>
2021-06-29 21:00:37 -0400

    added new ShowNotFound ErrorDisplay

---
+++ src/components/ShowNotFoundError.js
@@ -0,0 +1,17 @@
+const ShowNotFoundError = () => {
+
+    return(
+
+        <div>
+        
+        <p> 404 - Not Found </p>
+        <p> Please choose above tabs to find the Car's related information.</p>
+
+        </div>
+        
+
+        
+    )
+}
+
+export default ShowNotFoundError
