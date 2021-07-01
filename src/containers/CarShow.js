cebf7468f4283b63591fa3fdafb8dc52b2e2a4be
sunitee <suniteebajpai2@gmail.com>
2021-07-01 13:07:37 -0400

    added CarShow

---
+++ src/containers/CarShow.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+
+function CarShow(props){
+
+    return(
+        <div id="show-car">
+            Show Car: showCars={props.showCars()}
+        </div>
+    )
+}
+
+export default CarShow
